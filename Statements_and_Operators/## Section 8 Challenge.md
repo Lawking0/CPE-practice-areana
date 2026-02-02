8d0baae48e356a68126aa46511a4520bc1392ae9
LAWRENCE-NELSON-BARRY <Lawrencebarry44@gmail.com>
2026-02-02 17:26:08 +0100

    save1

---
+++ Statements_and_Operators/## Section 8 Challenge.md
@@ -0,0 +1,33 @@
+##  Section 8 Challenge
+
+For this program i will be using US dollars and cents 
+
+Write a Program that asks the user to enter the following 
+An integer representing the number of cents
+
+You may assume that number of cents entered is greater than or equal to zero 
+
+The program should then display how to provide that change to the user 
+
+In the US: 
+1 dollar is 100 cents
+1 quarter is 25 cents 
+1 dime is 10 cents
+1 nickel is 5 cents
+1 penny is 1 cent
+
+Here is a sample run: 
+
+Enter an amount in cents: 92 
+
+You can provide this change as follows: 
+dollars: 0 
+quarters: 3
+dimes:1
+nickels:1
+pennies:2
+
+Feel free to use your own curreny system 
+Also, think of how you might solve the problem using the modulo operator
+
+Have fun and test your
---
+++ Statements_and_Operators/My Section 8 Challenge Solutions.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+int main () {
+    
+
+    int kobo {0};
+    float Naira {0};
+long double cent{0};
+    float dollar {0};
+    long double quarter {0};
+    long double dime {0};
+    long double nickel {0};
+  long double penny {0};
+
+    // kobo rates to their respectively currency
+    /* Note that the rate i used is differnet from the current rate i used 1$ = ₦96.15*/
+   float Naira_kobo_rate {0.01};
+   long double kobo_cent_rate {96.15};
+  float kobo_dollar_rate {0.0104};
+    long double kobo_quarter_rate{41250};
+   long double  kobo_dime_rate {16500};
+   long double kobo_nickel_rate {8250};
+   long double kobo_penny_rate {1650};
+
+
+
+    //users requests 
+    std::cout << "Enter an amount in kobo"<<std::endl;
+    std::cin >> kobo;
+
+    // system brain
+    Naira_kobo_rate *= ( ( kobo )/1 );
+    Naira = Naira_kobo_rate;
+
+    long double kobo_cent = kobo;
+    kobo_cent /=kobo_cent_rate;
+     cent = kobo_cent;
+
+    kobo_dollar_rate *=(( kobo/1));
+    dollar = kobo_dollar_rate;
+
+    long double kobo_quarter = kobo;
+    kobo_quarter /=kobo_penny_rate;
+    quarter = kobo_quarter;
+
+    long double kobo_dime = kobo;
+    kobo_dime /= kobo_dime_rate;
+    dime = kobo_dime; 
+
+    long double kobo_nickel =kobo;
+    kobo_nickel /=kobo_nickel_rate;
+    nickel = kobo_nickel;
+
+    long double kobo_penny =kobo;
+    kobo_penny /=kobo_penny_rate;
+    penny = kobo_penny;
+
+std::cout<<" " <<std::endl;
+std::cout<<" " <<std::endl;
+
+    // output display 
+    std::cout <<"Kobo: "<< kobo << "kobo"<< std::endl;
+    std::cout <<"Naira: " << Naira << "₦"<<std::endl;
+    std::cout << "cents: " <<cent <<"" <<std::endl;
+    std::cout <<"dollars: "<< dollar <<"$" << std::endl;
+    
+    std::cout <<"quarters: " <<quarter << "₵" << std::endl;
+    std::cout <<"dimes: "<< dime << "¢"<< std::endl;
+    std::cout <<"nickels: "<< nickel <<"₵ " << std::endl;
+    std::cout <<"pennies: " << penny<< "p" << std::endl;
+    return 0;
+}
